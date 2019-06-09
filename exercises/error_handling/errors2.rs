254417ffe484f6df4317e55362a59b8ab6c0e64a
Rohan Prinja <rohan.prinja@gmail.com>
2019-06-09 01:47:57 -0700

    solutions up to and not including arc1.rs

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -21,8 +21,11 @@ use std::num::ParseIntError;
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
-
+    // one approach: using map(||)
+    // let qty = item_quantity.parse::<i32>();
+    // qty.map(|q| q * cost_per_item + processing_fee)
+    // another approach: using ?
+    let qty = item_quantity.parse::<i32>()?;
     Ok(qty * cost_per_item + processing_fee)
 }
 
